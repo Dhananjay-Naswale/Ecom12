06792ff692cb2f23587530ad838eafc388ac2a8a
Dhananjay Naswale <dhananjay.naswale811@gmail.com>
2025-01-22 16:13:09 +0530

    First V1

---
+++ ecom-main/src/main/java/com/codesnippet/ecom/aspect/LoggingAspect.java
@@ -0,0 +1,24 @@
+package com.codesnippet.ecom.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+    @Pointcut("execution(* com.codesnippet.ecom.Service.*.*(..))") // the pointcut expression
+    private void anyOldTransfer() {
+    }
+
+    @Around("anyOldTransfer()")
+    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("Aspect log called");
+        //method/jointpoint call
+        Object result = joinPoint.proceed();
+        System.out.println("Aspect after log called");
+        return result;
+    }
+}
---
+++ ecom-main/src/test/java/com/codesnippet/ecom/service/ProductServiceTest.java
@@ -0,0 +1,95 @@
+package com.codesnippet.ecom.service;
+
+import com.codesnippet.ecom.Entity.Product;
+import com.codesnippet.ecom.Repository.ProductRepository;
+import com.codesnippet.ecom.Service.ProductService;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductServiceTest {
+    private static Product product = null;
+    @Mock
+    ProductRepository productRepository;
+    @InjectMocks
+    ProductService productService;
+
+    @BeforeAll
+    public static void init() {
+        System.out.println("BeforeAll");
+        product = new Product();
+        product.setId(1);
+        product.setName("Book");
+        product.setDescription("harry potter");
+        product.setPrice(200);
+    }
+
+    @AfterAll
+    public static void Destroy() {
+        System.out.println("AfterAll");
+    }
+
+    @BeforeEach
+    public void initEachTest() {
+        System.out.println("BeforeEach");
+    }
+
+    @Test
+    void addProductShouldAddProductSuccessfully() {
+        when(productRepository.save(product)).thenReturn(product);
+
+        Product addedProduct = productService.addProduct(product);
+
+        assertNotNull(addedProduct);
+        assertEquals(product.getId(), addedProduct.getId());
+        assertEquals(product.getName(), addedProduct.getName());
+        assertTrue(product.getId() == 1);
+    }
+
+    @Test
+    void addProductShouldThrowExceptionForInvalidProductName() {
+        product.setName("");
+        RuntimeException runtimeException = assertThrows(RuntimeException.class, () -> {
+            productService.addProduct(product);
+        });
+        assertEquals("Invalid Name Of Product", runtimeException.getMessage());
+        verify(productRepository, never()).save(any(Product.class));
+    }
+
+    @Test
+    public void deleteProductShouldDeleteProductSuccessfully() {
+        doNothing().when(productRepository).deleteById(1);
+        productService.deleteProduct(1);
+        verify(productRepository, times(1)).deleteById(1);
+    }
+
+    @Test
+    void testPrivateMethod_validateProductName() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method validateProductName = ProductService.class.getDeclaredMethod("validateProductName", String.class);
+        validateProductName.setAccessible(true);
+        Boolean book = (Boolean) validateProductName.invoke(productService, "Book");
+        assertTrue(book);
+    }
+
+    @Test
+    void testPrivateMethod_validateProductNameIfNameIsInvalid() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method validateProductName = ProductService.class.getDeclaredMethod("validateProductName", String.class);
+        validateProductName.setAccessible(true);
+        Boolean book = (Boolean) validateProductName.invoke(productService, "");
+        assertFalse(book);
+    }
+
+    @AfterEach
+    public void cleanup() {
+        System.out.println("AfterEach");
+    }
+}
